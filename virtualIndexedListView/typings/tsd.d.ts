51948d71cff2b3a759e0e8cae11b8a84ba933fbc
Quinntyne Brown <qbrown@cetaris.com>
2015-09-19 19:08:43 -0400

    no need to track typings

--- virtualIndexedListView/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="lovefield/lovefield.d.ts" />
