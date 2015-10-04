032eea3f28dccc8980c18e049c2a94715872ab0a
Quinntyne Brown <qbrown@cetaris.com>
2015-10-04 09:20:48 -0400

    chore(examples):fix and clean up

--- examples/listDetail/listDetail.d.ts
+++ examples/listDetail/listDetail.d.ts
@@ -1,2 +1,4 @@
 ﻿/// <reference path="../../typings/typescriptapp.d.ts" />
+/// <reference path="../../node_modules/state-resolver/typings/typescriptapp.d.ts" />
+/// <reference path="../../node_modules/angular-data-service/typings/typescriptapp.d.ts" />
 
